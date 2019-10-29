29c26557f949f480ad62b98bae2dccd9a47ffc41
Antonio Raimundo <antoniorrm@Mac-mini-de-Eduardo.local>
2019-10-29 16:39:03 -0300

    teste deploy

--- src/index.js
+++ src/index.js
@@ -24,5 +24,5 @@ app.use(express.json())
 app.use(require('./router'))
 
 server.listen ( process.env.PORT || 3000, () => {
-    console.log('Server started on port 3000'); 
+    console.log('Server started on port '+process.env.PORT); 
 });
